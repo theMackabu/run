c9de383b9905be07633d88a4324c58097bc3a8f4
theMackabu <theMackabu@gmail.com>
2024-09-07 14:31:47 -0700

    add help command

---
+++ help.sh
@@ -0,0 +1,8 @@
+function module {
+	echo "<script_name> [args...]: Execute a script with optional arguments"
+	echo "\nAvailable commands:"
+	echo " - help: View this command"
+	echo " - list: List all available scripts"
+	echo " - info <script_name>: Display information about a specific script"
+	echo " - view <script_name>: Print script contents"
+}
